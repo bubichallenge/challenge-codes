41ecbad23ae45922e76a46f3ccdd9c8522035aff
Adrienn Szabo <adrienn.szabo@sztaki.mta.hu>
2015-10-02 10:45:27 +0200

    python style fix: spaces

--- challenge_data.py
+++ challenge_data.py
@@ -5,13 +5,13 @@ class ChallengeData:
   def __init__(self):
     self.daily_lists = {}
 
-  def read_from_file(self,file_name):
-    f = open(file_name,'r')
+  def read_from_file(self, file_name):
+    f = open(file_name, "r")
     for line in f:
       records = line.split(",")
       day = records[0]
       entity = records[1]
-      value= float(records[2])
+      value = float(records[2])
       if day not in self.daily_lists:
         self.daily_lists[day] = {}
       self.daily_lists[day][entity] = value
